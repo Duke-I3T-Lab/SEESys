b31d0f6690a1ca7ae9d270ec6d6f43a16872549e
hu_tianyi <tianyi.hu@duke.edu>
2024-11-04 18:32:26 +0800

    Create run_SLAM.sh

---
+++ VisualSLAM/run_SLAM.sh
@@ -0,0 +1,20 @@
+# ------------------------------------------------------------------
+# Usage: ./mono_inertial_tum_vi path_to_vocabulary path_to_settings path_to_image_folder_1 path_to_times_file_1 path_to_imu_data_1 
+
+#./Examples/Monocular/mono_tum_vi ./Vocabulary/ORBvoc.txt ./Examples/Monocular/SenseTime.yaml ./datasets/A0/camera/images/ ./Examples/Monocular/SenseTime_TimeStamps/A0.txt
+#./Examples/Monocular/mono_tum_vi ./Vocabulary/ORBvoc.txt ./Examples/Monocular/SenseTime.yaml ./datasets/A2/camera/images/ ./Examples/Monocular/SenseTime_TimeStamps/A2.txt
+
+
+#./Examples/Monocular/mono_tum_vi ./Vocabulary/ORBvoc.txt ./Examples/Monocular/SenseTime.yaml ./datasets/SenseTime/C10/camera/images/ ./Examples/Monocular/SenseTime_TimeStamps/C10.txt
+
+#./Examples/Monocular/mono_tum_vi ./Vocabulary/ORBvoc.txt ./Examples/Monocular/SenseTime.yaml ./datasets/SenseTime/C11/camera/images/ ./Examples/Monocular/SenseTime_TimeStamps/C11.txt
+
+#./Examples/Monocular/mono_tum_vi ./Vocabulary/ORBvoc.txt ./Examples/Monocular/SenseTime.yaml ./datasets/SenseTime/D8/camera/images/ ./Examples/Monocular/SenseTime_TimeStamps/D8.txt
+
+#./Examples/Monocular/mono_tum_vi ./Vocabulary/ORBvoc.txt ./Examples/Monocular/SenseTime.yaml ./datasets/SenseTime/A0/camera/images/ ./Examples/Monocular/SenseTime_TimeStamps/A0.txt
+
+./Examples/Monocular/mono_tum_vi ./Vocabulary/ORBvoc.txt ./Examples/Monocular/SenseTime.yaml ./datasets/SenseTime/A2/camera/images/ ./Examples/Monocular/SenseTime_TimeStamps/A2.txt
+
+#./Examples/Monocular/mono_tum_vi ./Vocabulary/ORBvoc.txt ./Examples/Monocular/SenseTime.yaml ./datasets/SenseTime/D10/camera/images/ ./Examples/Monocular/SenseTime_TimeStamps/D10.txt
+
+# ./Examples/Monocular/mono_tum_vi ./Vocabulary/ORBvoc.txt ./Examples/Monocular/SenseTime.yaml ./datasets/A3/camera/images/ ./Examples/Monocular/SenseTime_TimeStamps/A3.txt
